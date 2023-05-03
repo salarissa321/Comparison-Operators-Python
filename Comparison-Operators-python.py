2aad9b0cd4153734a3a8b3bfbea857478b53138c
salarissa321 <salarissa321@gmail.com>
2023-05-03 16:49:07 +0200

    add Comparison-Operator-Python

---
+++ Comparison-Operators-python.py
@@ -0,0 +1,74 @@
+
+
+
+
+# -----------------------------------
+# ---- comparison Operators-----
+# -----------------------------------
+# [==] Equal
+# [!=] Not Equal
+# [>] Greater Than
+# [<] Less than
+# [>=] Greater Than or Equal
+# [<=] less tha or Equal
+#----------------------------------------
+
+
+
+# Equal  
+
+print(100 == 100) # True
+print(100 == 200) # False
+print(100 == 100.00) # True
+
+# print("-" * 50) # --------------------------------------------------
+
+# Not Equal
+
+print(100 != 100) # False
+print(100 != 200) # True
+print(100 != 100.00) # False
+
+print("-" * 50) # --------------------------------------------------
+
+
+
+#  Greater Than
+
+print(100 > 100) # False
+print(100 > 200) #  False
+print(100 > 100.00) # False 
+print(100 > 40) # True
+
+print("-" * 50) # --------------------------------------------------
+
+# Less than
+
+print(100 < 100) # False
+print(100 < 200) #  True
+print(100 < 100.00) #  False
+print(100 < 40) #  False
+
+print("-" * 50) # --------------------------------------------------
+
+
+#  Greater Than or Equal
+
+print(100 >= 100) # True
+print(100 >= 200) #  False
+print(100 >= 100.00) # True
+print(100 >= 40) # True
+
+print("-" * 50) # --------------------------------------------------
+
+
+#  Less Than or Equal
+
+print(100 <= 100) # True
+print(100 <= 200) #  True
+print(100 <= 100.00) # True
+print(100 <= 40) # False
+
+print("-" * 50) # --------------------------------------------------
+
+
